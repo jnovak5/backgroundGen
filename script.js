20c50333a4a532dfb2279aea4b2dc97674ac5e45
Jonathon Novak <114377152+jnovak5@users.noreply.github.com>
2023-09-09 09:43:35 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var color3 = document.querySelector(".color3");
+var body = document.getElementById("gradient");
+
+function pageLoad() {
+	setGradient();
+}
+
+function setGradient() {
+	body.style.background = "linear-gradient(to right, "
+	+ color1.value 
+	+ ", " 
+	+ color2.value
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+}
+
+function randomHex() {
+	var hexNumbers = "0123456789ABCDEF";
+	var randomNumber = "#"
+
+	for (var i=0; i < 6; i++) {
+		randomNumber += hexNumbers[(Math.floor(Math.random() * 16))];
+	}
+
+	return randomNumber;
+}
+
+function randomGradient() {
+	color1.value = randomHex();
+	color2.value = randomHex();
+	
+	body.style.background = "linear-gradient(to right, "
+	+ color1.value
+	+ ", " 
+	+ color2.value
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+}
+
+pageLoad();
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+
+color3.addEventListener("click", randomGradient);
